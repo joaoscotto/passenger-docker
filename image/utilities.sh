4c6323eebcc121d5d18bd0d72e37c085320bafac
joao.scotto <joao.scotto@gmail.com>
2023-10-30 15:04:35 -0300

    add wkhtmltopdf and nodejs

--- image/utilities.sh
+++ image/utilities.sh
@@ -10,3 +10,24 @@ run minimal_apt_get_install git
 run minimal_apt_get_install netbase
 ## utilities needed to add apt ppas
 run minimal_apt_get_install curl gnupg ca-certificates
+
+# Install wkhtmltopdf
+run minimal_apt_get_install libcurl4-openssl-dev libfontenc1 xfonts-75dpi xfonts-base xfonts-encodings xfonts-utils fontconfig \
+                            fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 \
+                            libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 \
+                            libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 \
+                            libstdc++6 libx11-6 libxtst6 lsb-release wget xdg-utils
+
+#amd64
+RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb \
+  && dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb \
+  && rm wkhtmltox_0.12.6-1.focal_amd64.deb
+
+#arm64 https://github.com/wkhtmltopdf/packaging/issues/98
+RUN wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_arm64.deb \
+  && dpkg -i wkhtmltox_0.12.6.1-2.jammy_arm64.deb \
+  && rm wkhtmltox_0.12.6.1-2.jammy_arm64.deb
+
+# Nodejs
+run curl -sL https://deb.nodesource.com/setup_16.x | bash -
+minimal_apt_get_install nodejs
